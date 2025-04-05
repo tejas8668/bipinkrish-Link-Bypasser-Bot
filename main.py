18c96c05e8c4d4b915994e656a9378f36f5ccb22
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-04-05 19:30:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -545,8 +545,8 @@ async def get_token(user_id: int, bot_username: str) -> str:
         return verification_link
 
 def shorten_url_link(url):
-    api_url = 'https://arolinks.com/api'
-    api_key = '180027087e13f4a147d7615e8ac5a8d93240050c'
+    api_url = 'https://adlinkfly.in/api'
+    api_key = '517af4c4953d46b68dd240cfd168fe27b49a58ad'
     params = {
         'api': api_key,
         'url': url
